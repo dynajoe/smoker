2dc28ba7e7c58670358189d83997d8d9ff55fe5c
Joe Andaverde <joe@andaverde.net>
2012-12-17 22:48:04 -0600

    Updated data collection to work on osx

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,38 @@
-var serialport = require("serialport");
+var serialport = require('serialport');
 var SerialPort = serialport.SerialPort
+var child = require('child_process');
+var collectedData = [];
+var buffer = [];
 
-var serialPort = new SerialPort("COM5", {
-  baudrate: 9600,
-  parser: serialport.parsers.readline("\n") 
+child.exec('ls /dev | grep cu.usbserial', function (err, stdout) {
+	
+	var ports = stdout.trim().split('\n');
+
+	if (ports.length > 0) {
+		var serialPort = new SerialPort('/dev/' + ports[0], {
+		  baudrate: 9600,
+		  parser: serialport.parsers.readline('\n') 
+		});	
+
+		serialPort.on('data', function (data) {
+			var parts = data.trim().split(',');
+
+			var point = {
+				temp: parseInt(parts[0]),
+				outsideTemp: parseInt(parts[1]),
+				isOn: parseInt(parts[2]) === 1,
+				time: Date.now(),
+				threshold: parseInt(parts[4]),
+				target: parseInt(parts[3])
+			};
+
+			collectedData.push(point);
+			buffer.push(point);
+		});
+	} else {
+		console.log('Unable to connect to XBEE!');
+	}
 });
-
-collectedData = [];
 
 module.exports = function (app, io) {
 	io.sockets.on('connection', function (socket) {
@@ -14,29 +40,24 @@ module.exports = function (app, io) {
 			socket.emit('history', collectedData);
 		});
 
-		socket.on('command', function(command) {
-			serialPort.write(command);
+		socket.on('time', function (clientTime) {
+			var now = Date.now();
+			var diff = clientTime - now;
+
+			socket.emit('time', { time: now, diff: diff });
+		});
+
+		socket.on('command', function (command) {
+			serialPort.write(command.trim() + '\n');
 		});
 	});
-	
-	serialPort.on('data', function (data) {
-		var matches = data.match(/(\w*):\s*(.*)/);
-		
-		if (!matches) {
-			return;
-		}
 
-		var key = matches[1].toLowerCase().trim();
-		var value = matches[2];
-
-		if (key === 'temp') {
-			var point = { time: new Date().getTime(), temp: value };
-			collectedData.push(point);
-			io.sockets.emit('temp', point);
-		} else {
-			io.sockets.emit(key, value);
+	setInterval(function () {
+		if (buffer.length > 0) {
+			io.sockets.emit('data', buffer);
+			buffer = [];
 		}
-	});
+	}, 2000);
 
    app.get('/', function (req, res) {
       res.render('index', { title: 'Smoker!' });
