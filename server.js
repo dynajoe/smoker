a781681b68545625076a5f9419ae82178126e69b
Joe Andaverde <joe@andaverde.net>
2014-05-31 12:14:02 -0500

    renamed variables

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
+var http_server = require('http').createServer(app);
+var io = require('socket.io').listen(http_server);
 var config = require('./config');
 var Smoker = require('./lib/smoker');
 var logger = require('winston');
@@ -22,21 +22,23 @@ app.configure(function () {
    }));
 });
 
-var server = mongodb.Server('localhost', '27017');
-var db = mongodb.Db('smoker', server, { safe: true });
+var mongo_server = mongodb.Server('localhost', '27017');
+var mongo_db = mongodb.Db('smoker', mongo_server, { safe: true });
 
-db.open(function (err) {
+mongo_db.open(function (err) {
    if (err) throw err;
 
-   var smoker = new Smoker(config, db);
+   var smoker = new Smoker(config, mongo_db);
 
    smoker.start()
    .then(function () {
+      logger.info('start complete');
       app.set('smoker', smoker);
       app.set('config', config);
       require('./routes/index')(app);
-      server.listen(app.get('port'));
-      logger.info('Server listening on port ' + app.get('port'));
+      logger.info('Starting http server on port %d', app.get('port'));
+      http_server.listen(app.get('port'));
+      logger.info('Server listening on port %d', app.get('port'));
    })
    .fail(function (e) {
       logger.error('Unable to start smoker', e);
