b82e525bb52e3e67b59f7a953ab24f2cc839ab3d
Joe Andaverde <joe@andaverde.net>
2014-11-10 20:27:17 -0600

    fixes for newer packages

--- server.js
+++ server.js
@@ -3,14 +3,13 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
-app.configure(function () {
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(require('stylus').middleware(__dirname + '/public'));
-  app.use(express.static(__dirname + '/public'));
-  app.use(require('connect-assets')());
-});
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(require('stylus').middleware(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
+app.use(require('connect-assets')());
 
 app.set('io', io);
 
