a5fe2dd2e3cc4047833b75475d30c5227dfaf812
Joe Andaverde <joe.andaverde@gmail.com>
2015-09-05 12:16:47 -0500

    Modifications to config to work on windows

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
-var express = require('express');
-var app = express();
+var Config = require('./lib/config');
+var Smoker = require('./lib/smoker');
+var Logger = require('winston');
+var Express = require('express');
+
+Logger.setLevels(Logger.config.syslog.levels);
+Logger.level = Config.log.level;
+
+var app = Express();
 var http_server = require('http').createServer(app);
 var io = require('socket.io').listen(http_server, { log: true });
-var config = require('./lib/config');
-var Smoker = require('./lib/smoker');
-var logger = require('winston');
-
-logger.setLevels(logger.config.syslog.levels);
-
-logger.info('NODE_ENV: ' + process.env.NODE_ENV);
 
 app.configure(function () {
    app.set('port', process.env.PORT || 3000);
    app.set('io', io);
    app.set('views', __dirname + '/views');
    app.set('view engine', 'jade');
-   app.use(express.static(__dirname + '/public/'));
-   app.use(express.static(__dirname + '/bower_components/'));
+   app.use(Express.static(__dirname + '/public/'));
+   app.use(Express.static(__dirname + '/bower_components/'));
    app.use(require('connect-assets')({
       helperContext: app.locals,
       paths: ['public/css']
    }));
 });
 
-app.set('config', config);
+app.set('config', Config);
 require('./routes/index')(app);
-logger.info('Starting http server on port %d.', app.get('port'));
-http_server.listen(app.get('port'));
-logger.info('Server listening on port %d.', app.get('port'));
+
+Logger.info('NODE_ENV: ' + process.env.NODE_ENV);
+Logger.info('Starting http server on port %d.', app.get('port'));
+
+http_server.listen(app.get('port'), function () {
+   Logger.info('Server listening on port %d.', app.get('port'));   
+});
+
